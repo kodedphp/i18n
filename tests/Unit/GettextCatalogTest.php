ecb22835e49c753c2a0ccf521f33033e430c5282
Mihail Binev <kodeart@gmail.com>
2022-01-25 14:46:31 +0100

    - more tests

--- tests/Unit/GettextCatalogTest.php
+++ tests/Unit/GettextCatalogTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Koded\I18n\Unit;
 
-use Koded\I18n\{ArrayCatalog, GettextCatalog, I18n, I18nCatalog};
+use Koded\I18n\{ArrayCatalog, GettextCatalog, I18n, I18nCatalog, NoCatalog};
 use Koded\Stdlib\Config;
 use PHPUnit\Framework\TestCase;
 
@@ -10,19 +10,79 @@ class GettextCatalogTest extends TestCase
 {
     public function test_supported_locales()
     {
-        $this->assertSame(
-            "A lazy fox's been jumped by a quick brown dog",
-            __('quick-fox', ['fox', 'dog'], 'en_US')
-        );
-
         $this->assertInstanceOf(
-            ArrayCatalog::class,
-            I18n::catalogs()['en_US']
+            GettextCatalog::class,
+            I18n::catalogs()['de_DE']
         );
 
         $this->assertSame(
             'Schneller brauner Fuchs springt über den faulen Hund',
             __('quick-fox', ['Fuchs', 'Hund'])
+        );
+
+        $this->assertSame(
+            'This string is passed through',
+            __('This string is passed through', [])
+        );
+    }
+
+    public function test_unsupported_locales()
+    {
+        $this->assertSame(
+            "A lazy fox's been jumped by a quick brown dog",
+            __('quick-fox', ['fox', 'dog'], 'en_US'),
+            'Translated because en_US.php exist'
+        );
+
+        $this->assertInstanceOf(
+            ArrayCatalog::class,
+            I18n::catalogs()['en_US'],
+            'Fallback to ArrayCatalog because en_US.php exist'
+        );
+    }
+
+    public function test_fallback()
+    {
+        $this->assertSame(
+            'quick-fox',
+            __('quick-fox', ['fox', 'dog'], 'mk_MK'),
+            'No mk_MK translation, the string is passed as-is'
+        );
+
+        $this->assertSame(
+            'quick-fox',
+            __('quick-fox', ['fox', 'dog'], 'it_IT'),
+            'No it_IT translation, the string is passed as-is'
+        );
+
+        $this->assertSame(
+            'This string is passed through',
+            __('This string is passed through', [], 'mk_MK'),
+            'No ml_MK translation, the string is passed as-is'
+        );
+
+        $this->assertSame(
+            'This string is passed through',
+            __('This string is passed through', [], 'fr_FR'),
+            'No fr_FR translation, the string is passed as-is'
+        );
+
+        $this->assertInstanceOf(
+            GettextCatalog::class,
+            I18n::catalogs()['mk_MK'],
+            'mk_MK locale is supported'
+        );
+
+        $this->assertInstanceOf(
+            GettextCatalog::class,
+            I18n::catalogs()['it_IT'],
+            'it_IT locale is supported'
+        );
+
+        $this->assertInstanceOf(
+            NoCatalog::class,
+            I18n::catalogs()['fr_FR'],
+            'fr_FR locale is NOT supported, no fr_FR.php, fallback to NoCatalog'
         );
     }
 
